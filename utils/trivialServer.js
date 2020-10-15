e16df1ea47a2858f1502cd45b4c5efc064401c17
Marco Ng <marcong95@163.com>
2020-10-15 20:27:34 +0800

    add trivial server

---
+++ utils/trivialServer.js
@@ -0,0 +1,24 @@
+const { join } = require('path')
+
+const Koa = require('koa')
+const cors = require('@koa/cors')
+const body = require('koa-body')
+const route = require('koa-route')
+
+const app = new Koa()
+
+app.use(cors())
+app.use(body())
+
+const handleRequest = ctx => {
+  console.log('QUERY:', ctx.request.query)
+  console.log(' TYPE:', ctx.headers['content-type'])
+  console.log(' BODY:', ctx.request.body)
+  console.log()
+  ctx.body = { success: true }
+}
+app.use(route.get('*', handleRequest))
+app.use(route.post('*', handleRequest))
+
+app.listen(4321)
+console.log('Listening on port 4321.')
