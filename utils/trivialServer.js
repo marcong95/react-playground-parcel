eb7eba0663f8293b7e0b3323eae8912d94c13641
Marco Ng <marcong95@163.com>
2020-10-28 16:01:41 +0800

    add time and method logging in trivialServer

--- utils/trivialServer.js
+++ utils/trivialServer.js
@@ -9,10 +9,12 @@ app.use(cors())
 app.use(body())
 
 const handleRequest = ctx => {
-  console.log('  URL:', ctx.request.url)
-  console.log('QUERY:', ctx.request.query)
-  console.log(' TYPE:', ctx.headers['content-type'])
-  console.log(' BODY:', ctx.request.body)
+  console.log('  TIME:', new Date().toString())
+  console.log('METHOD:', ctx.request.method)
+  console.log('   URL:', ctx.request.url)
+  console.log(' QUERY:', ctx.request.query)
+  console.log('  TYPE:', ctx.headers['content-type'])
+  console.log('  BODY:', ctx.request.body)
   console.log()
   ctx.body = { success: true }
 }
